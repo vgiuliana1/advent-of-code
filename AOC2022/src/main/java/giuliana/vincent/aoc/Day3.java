121a344c621d1f83b7651f6acf648f8f3dafe9a6
Vincent Giuliana <vincent@MacBook-Air.local>
2022-12-05 22:04:51 -0500

    Day 3 Part 2 solution

--- AOC2022/src/main/java/giuliana/vincent/aoc/Day3.java
+++ AOC2022/src/main/java/giuliana/vincent/aoc/Day3.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -12,14 +13,29 @@ public class Day3 {
     public static void solution() {
         File inputFile = new File("AOC2022/src/main/resources/Day3-input.txt");
         try (Stream<String> lines = Files.lines(Paths.get(inputFile.getAbsolutePath()))) {
-            int sum = 0;
 
-            for (String line : lines.collect(Collectors.toList())) {
+            List<String> linesList = lines.collect(Collectors.toList());
+
+            /* Part 1 */
+            int sum = 0;
+            for (String line : linesList) {
                 String[] splitLine = new String[]{line.substring(0, line.length() / 2), line.substring(line.length() / 2)};
                 sum += getLeftoverAmount(splitLine);
             }
 
-            System.out.println("Sum: " + sum);
+            System.out.println("Sum (Part 1): " + sum);
+
+            /* Part 2 */
+            int sumP2 = 0;
+            String[] groupLines = new String[3];
+            for (int i = 0; i <= linesList.size() - 3; i += 3) {
+                groupLines[0] = linesList.get(i);
+                groupLines[1] = linesList.get(i + 1);
+                groupLines[2] = linesList.get(i + 2);
+                sumP2 += findCommonCharacter(groupLines);
+            }
+
+            System.out.println("Sum (Part 2): " + sumP2);
 
         } catch (IOException e) {
             System.out.println("Exception: " + e.getMessage());
@@ -40,5 +56,17 @@ public class Day3 {
         }
 
         throw new RuntimeException("WRONG");
+    }
+
+    private static int findCommonCharacter(String[] groupLines) {
+        for (String s : groupLines[0].split("")) {
+            if (groupLines[1].contains(s) && groupLines[2].contains(s)) {
+                char c = s.toCharArray()[0];
+                if (!Character.isUpperCase(c))
+                    return c - 96;
+                else return c - 38;
+            }
+        }
+        throw new RuntimeException("WRONG P2");
     }
 }
